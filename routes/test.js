33f202da2435c64e5b4e1386a7e5970f2cb811b3
mukaisam-cpu <mukaisam@uw.edu>
2026-05-12 18:53:42 -0700

    Stuff works

--- routes/test.js
+++ routes/test.js
@@ -2,4 +2,8 @@ import { Router } from 'express';
 
 const router = Router();
 
+router.get('/', async (req, res) => {
+    return res.status(200).send("This is text.");
+})
+
 export default router;
