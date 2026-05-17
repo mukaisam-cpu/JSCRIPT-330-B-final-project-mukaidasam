7bde1b88925c389f7bf2409e041d1fddb5e4c157
mukaisam-cpu <mukaisam@uw.edu>
2026-05-17 15:42:19 -0700

    Index update

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+import auth from './auth'
+import bookmarks from './bookmarks'
+import playlists from './playlists'
 
 const router = Router();
+
+router.use('/auth', auth);
+router.use('./bookmarks', bookmarks);
+router.use('./playlists', playlists);
 
 export default router;
