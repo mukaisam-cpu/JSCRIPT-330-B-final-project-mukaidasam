e50c63e1c81890b47912ab83087f95a1373df393
mukaisam-cpu <mukaisam@uw.edu>
2026-05-17 14:24:15 -0700

    Playlist model

--- models/playlist.js
+++ models/playlist.js
@@ -1,5 +1,15 @@
 import mongoose from "mongoose";
 
-const playlistSchema = new mongoose.Schema({});
+const playlistSchema = new mongoose.Schema({
+    userId = {
+        type: mongoose.Schema.ObjectId,
+        ref: User,
+        required: true,
+    },
+    name = {type: String, required: true},
+    games = {type: Array, required: false}
+});
+
+playlistSchema.index({ name: 'text'});
 
 export default mongoose.model('Playlist', playlistSchema);
