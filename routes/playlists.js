6475c7330b079b417d002a8083519a98be9660fc
mukaisam-cpu <mukaisam@uw.edu>
2026-05-17 15:39:59 -0700

    Playlists route stubs

--- routes/playlists.js
+++ routes/playlists.js
@@ -6,4 +6,12 @@ router.get('/', async (req, res) => {
     return res.status(200).send("Playlists get");
 })
 
+router.post('/', async (req, res) => {
+    return res.status(200).send("Playlists post");
+})
+
+router.delete('/:id', async (req, res) => {
+    return res.status(200).send("Playlists delete");
+})
+
 export default router;
