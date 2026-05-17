334b674170b01f6953796a9e19d4a39f135eb03a
mukaisam-cpu <mukaisam@uw.edu>
2026-05-17 15:00:46 -0700

    Playlist tests

--- routes/playlists.test.js
+++ routes/playlists.test.js
@@ -3,4 +3,45 @@ describe('/playlists', () => {
     afterAll(testUtils.stopDB);
 
     afterEach(testUtils.clearDB);
+
+    describe('Before login', () => {
+        describe('GET /', () => {
+
+        });
+
+        describe('POST /', () => {
+
+        });
+    })
+
+    describe('After login', () => {
+        describe('GET /', () => {
+            it('should return all playlists for the current user', async () => {
+
+            });
+        });
+
+        describe('GET /:id', () => {
+            it('should return the specified playlist if it was created by the user', async () => {
+
+            });
+
+            it('should return an error if the playlist does not belong to the user', async () => {
+
+            });
+            // Admin authorization to view all playlists, perhaps?
+        });
+
+        describe('PUT /', () => {
+            it('should save the selected playlist', async () => {
+
+            });
+        });
+
+        describe('DELETE /:id', () => {
+            it('should delete the selected playlist', async () => {
+
+            });
+        })
+    })
 })
