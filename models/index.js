19e3b2ee2f2b6961cacd0fc77028380749f44170
mukaisam-cpu <mukaisam@uw.edu>
2026-05-17 14:18:24 -0700

    Hooked up other models to index

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 import User from './user'
+import Token from './token'
+import Playlist from './playlist'
+
+// Future plans: I'm not 100% sure if I'll need a model for games
+// I'd be acquiring game data from the RetroAchievements API, but I'm not sure if it'd
+// be safer to acquire that seperately (do I need to worry about users implementing false data?)
+// or if I'd want to cache that information to decrease the calls I would make
 
 export default {
-    User
+    User,
+    Token,
+    Playlist
 }
