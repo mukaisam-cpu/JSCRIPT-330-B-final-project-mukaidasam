369996c8ffbe9c3a193dd4d857f41b195c1b2791
mukaisam-cpu <mukaisam@uw.edu>
2026-05-17 14:15:07 -0700

    Figured out how to get the database to show up in Compass

--- routes/test.js
+++ routes/test.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import * as User from '../daos/user';
 
 const router = Router();
 
 router.get('/', async (req, res) => {
+    await User.createUser({ password: 'notapassword', email: 'test' });
     return res.status(200).send("This is text.");
 })
 
