d0c738a23e63c14c329b279d926dc3248139e562
mukaisam-cpu <mukaisam@uw.edu>
2026-05-29 16:13:21 -0700

    Fixed index routes

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ import playlists from './playlists'
 const router = Router();
 
 router.use('/auth', auth);
-router.use('./bookmarks', bookmarks);
-router.use('./playlists', playlists);
+router.use('/bookmarks', bookmarks);
+router.use('/playlists', playlists);
 
 export default router;
